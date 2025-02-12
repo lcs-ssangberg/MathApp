0d27563e3d6fd7e88327559554465241a9f09bc8
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-12 09:54:34 -0500

    created the tabview

--- MathApp/Views/LandingView.swift
+++ MathApp/Views/LandingView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct LandingView: View {
+    
+    @State private var selectedTab: Int = 1
+    
     var body: some View {
-        NavigationStack {
+        
+        TabView(selection: $selectedTab) {
+            
+            AdditionView()
+                .tabItem {
+                    Image(systemName: "plus")
+                    Text("Addition")
+                }
+                .tag(1)
+
+            SubtractionView()
+                .tabItem {
+                    Image(systemName: "minus")
+                    Text("Subtraction")
+                }
+                .tag(2)
+            
+            MultiplicationView()
+                .tabItem {
+                    Image(systemName: "multiply")
+                    Text("Multiplication")
+                }
+                .tag(3)
+            
+            DivisionView()
+                .tabItem {
+                    Image(systemName: "divide")
+                    Text("Division")
+                }
+                .tag(4)
+            
+            SquareView()
+                .tabItem {
+                    Image(systemName: "2.square")
+                    Text("Square")
+                }
+                .tag(5)
             
         }
     }
--- MathApp/Views/SquareView.swift
+++ MathApp/Views/SquareView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SquareView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Square")
     }
 }
 
