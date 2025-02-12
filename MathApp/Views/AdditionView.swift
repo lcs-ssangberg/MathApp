7ad54fb295dd0db65a3201ebb8116dad43df89ca
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-12 16:43:07 -0500

    created a rudimentary additionView, cant push as off wifi

--- MathApp/Views/AdditionView.swift
+++ MathApp/Views/AdditionView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct AdditionView: View {
-    var body: some View {
-        Text("Addition")
+    // MARK: Stored properties
+    @State var number1: Int = 1
+    @State var number2: Int = 1
+    
+    // MARK: Computed properties
+    var additionSolution: Int {
+            return number1 + number2
+        }
+        
+        var body: some View {
+            VStack(alignment: .trailing) {
+                Text("\(number1)")
+                
+                HStack {
+                    Image(systemName: "plus")
+                    Text("\(number2)")
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image(systemName: "equal")
+                    Text("\(additionSolution)")
+                }
+                
+            }
     }
 }
 
--- MathApp/Views/SquareView.swift
+++ MathApp/Views/SquareView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct SquareView: View {
-    var body: some View {
-        Text("Square")
-    }
+        // MARK: Stored properties
+        @State var base: Int = 1
+            
+        // MARK: Computed properties
+        var squared: Int {
+                return base * base
+            }
+            
+            
+            var body: some View {
+                VStack {
+                    
+                    Spacer()
+                    
+                    HStack(alignment: .top) {
+                        
+                        Text("(\(base))")
+                            .font(.system(size: 96))
+         
+                        Text("2")
+                            .font(.system(size: 44))
+         
+                        Text("=")
+                            .font(.system(size: 96))
+         
+                        Text("\(squared)")
+                            .font(.system(size: 96))
+                    }
+                        
+                    Stepper(value: $base, label: {
+                        Text("Base")
+                    })
+                    
+                    Spacer()
+                }
+                .padding()
+        }
 }
 
 #Preview {
