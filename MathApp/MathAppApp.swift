5fc6d0319aaa082754c0004f51530aac62d61bfa
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-12 09:16:43 -0500

    Initial Commit

---
+++ MathApp/MathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathAppApp.swift
+//  MathApp
+//
+//  Created by Sebastian on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct MathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
